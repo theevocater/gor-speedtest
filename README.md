777be989e9d419d939e5e12463c9f415c370af9b
David Taylor <tinystatemachine@gmail.com>
2015-09-09 20:57:06 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Performance Logging Gor Middleware
+A tiny [gor middleware](http://github.com/buger/gor#middleware) that logs request latency to graphite.
+
---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/hex"
+	"flag"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/dt/go-metrics-reporting"
+)
+
+const (
+	RequestFlag          = '1'
+	ResponseFlag         = '2'
+	ReplayedResponseFlag = '3'
+)
+
+type SettingDefs struct {
+	src      string
+	dst      string
+	graphite string
+	prefix   string
+}
+
+var Settings SettingDefs
+
+func main() {
+	flag.StringVar(&Settings.src, "src", "src", "name for traffic src")
+	flag.StringVar(&Settings.dst, "dst", "dst", "name for traffic dst")
+	flag.StringVar(&Settings.graphite, "graphite", "", "name for traffic dst")
+	flag.StringVar(&Settings.prefix, "prefix", "", "prefix for reported timings")
+	flag.Parse()
+
+	report.NewRecorder().ReportTo(Settings.graphite, Settings.prefix).SetAsDefault()
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		encoded := scanner.Bytes()
+
+		buf := make([]byte, len(encoded)/2)
+		hex.Decode(buf, encoded)
+		kind := buf[0]
+		headerSize := bytes.IndexByte(buf, '\n') + 1
+		header := buf[:headerSize-1]
+		meta := bytes.Split(header, []byte(" "))
+		//id := string(meta[1])
+		ts, _ := strconv.ParseInt(string(meta[2]), 10, 64)
+
+		if kind == RequestFlag {
+			os.Stdout.Write(encoded)
+		}
+
+		if kind == ResponseFlag {
+			report.Time(Settings.src, time.Duration(ts))
+		} else if kind == ReplayedResponseFlag {
+			report.Time(Settings.src, time.Duration(ts))
+		}
+	}
+}
